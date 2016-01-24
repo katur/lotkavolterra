66ab77c26479c2216ac44fd5fed045e335f27bf7
Katherine Erickson <katherine.erickson@gmail.com>
2016-01-24 17:42:32 -0500

    Barbones interaction functions

---
+++ interactions.py
@@ -0,0 +1,54 @@
+import random
+from coins import CoinFlip
+from models import Group
+
+
+GROWTH_RATE = 1.333
+COMPETITIVE_COIN_WEIGHT = 0.667
+
+
+def change_group_if_colony(x):
+    if not x.is_colony():
+        return False
+
+    x.set_group(random.choice([Group.PACK, Group.HERD])
+    return True
+
+
+def compete(pack, herd):
+    f = flip_coin(weight=COMPETITIVE_COIN_WEIGHT)
+
+    if f == CoinFlip.HEADS:
+        pack.increase_size(GROWTH_RATE)
+        herd.decrease_size(GROWTH_RATE)
+    else:
+        pack.decrease_size(GROWTH_RATE)
+        herd.increase_size(GROWTH_RATE)
+
+
+def interact(x, y):
+    # If a or b is a colony, set temporarily to a pack or herd
+    x_was_colony = change_group_if_colony(x)
+    y_was_colony = change_group_if_colony(y)
+
+    # Now the interaction falls into 4 cases
+    if x.is_herd() and y.is_herd():
+        x.increase_size(GROWTH_RATE)
+        y.increase_size(GROWTH_RATE)
+
+    elif x.is_pack() and y.is_pack():
+        x.decrease_size(GROWTH_RATE)
+        y.decrease_size(GROWTH_RATE)
+
+    elif x.is_pack():
+        compete(pack=x, herd=y)
+
+    else:
+        compete(pack=y, herd=x)
+
+    # Set colonies back
+    if x_was_colony:
+        x.set_group(Group.COLONY)
+
+    if y_was_colony:
+        y.set_group(Group.COLONY)
