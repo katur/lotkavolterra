39c4bfb86ddb55fcef0971d99e8a7ca7d665fb69
Katherine Erickson <katherine.erickson@gmail.com>
2016-01-24 20:04:19 -0500

    Add docstrings

--- coins.py
+++ coins.py
@@ -3,10 +3,17 @@ from enum import Enum
 
 
 class Flip(Enum):
+    """The result of a coin flip."""
     HEADS, TAILS = range(2)
 
 
 def flip_coin(weight=0.5):
+    """
+    Flip a coin.
+
+    Optionally pass in weight, where weight is the probability of flipping
+    Heads. Defaults to a fair coin.
+    """
     r = random.random()
     if r < weight:
         return Flip.HEADS
