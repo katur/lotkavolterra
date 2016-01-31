ae0138e259741002b83804ce163cacf6f97f6c49
Katherine Erickson <katherine.erickson@gmail.com>
2016-01-31 16:18:51 -0500

    Pass num_seats and population_size as GET param

--- launch.py
+++ launch.py
@@ -9,17 +9,31 @@ from localsettings import DEBUG
 app = Flask(__name__)
 
 
-# Test settings
+# Default sizes
+
 DEFAULT_NUM_GENERATIONS = 5
+DEFAULT_NUM_SEATS = 10
 DEFAULT_POPULATION_SIZE = 100
-PEOPLE = ('Alice', 'Bob', 'Carol', 'Django', 'Erlich', 'Freddy',
-          'Georgia', 'Heidi', 'Indigo', 'Jack',)
+
 
 # Test simulations
+
 RANDOM = 'random'
 ALTERNATING = 'alternating'
 HALVES = 'halves'
 SIMULATIONS = (RANDOM, ALTERNATING, HALVES,)
+
+
+# Test people
+
+PEOPLE = ('Alice', 'Bob', 'Carol', 'Django', 'Erlich', 'Freddy',
+          'Georgia', 'Heidi', 'Indigo', 'Jack',)
+
+def get_person(i):
+    try:
+        return PEOPLE[i]
+    except IndexError:
+        return 'Person{}'.format(i)
 
 
 @app.route("/")
@@ -35,21 +49,16 @@ def list_simulations():
     return render_template('list_simulations.html', **context)
 
 
-@app.route("/test-simulation/<name>/")
-def test_simulation(name):
+@app.route("/test-simulation/<simulation>/")
+def test_simulation(simulation):
     # Enable command line DEBUG logging
     logging.basicConfig(level=logging.DEBUG)
 
-    num_generations = request.args.get('num_generations',
-                                       DEFAULT_NUM_GENERATIONS)
-    try:
-        num_generations = int(num_generations)
-    except TypeError():
-        num_generations = DEFAULT_NUM_GENERATIONS
+    num_seats, population_size, num_generations = parse_get_params()
 
     # Create and populate Test table
     table = Table('Test')
-    populate_test_table(table, name)
+    populate_test_table(table, simulation, num_seats, population_size)
 
     # Run the simulation
     table.all_seats_interact(num_generations=num_generations)
@@ -63,27 +72,47 @@ def test_simulation(name):
 
 
 # Helpers
-def populate_test_table(table, name):
-    if name == RANDOM:
-        for person in PEOPLE:
-            table.insert(person, get_random_group(),
-                         DEFAULT_POPULATION_SIZE)
 
-    elif name == ALTERNATING:
-        for i, person in enumerate(PEOPLE):
+def parse_get_params():
+    try:
+        num_seats = int(request.args['num_seats'])
+    except Exception:
+        num_seats = DEFAULT_NUM_SEATS
+
+    try:
+        population_size = int(request.args['population_size'])
+    except Exception:
+        population_size = DEFAULT_POPULATION_SIZE
+
+    try:
+        num_generations = int(request.args['num_generations'])
+    except Exception:
+        num_generations = DEFAULT_NUM_GENERATIONS
+
+    return (num_seats, population_size, num_generations)
+
+
+def populate_test_table(table, simulation, num_seats=DEFAULT_NUM_SEATS,
+                        population_size=DEFAULT_POPULATION_SIZE):
+    for i in range(num_seats):
+        person = get_person(i)
+
+        if simulation == RANDOM:
+            table.insert(person, get_random_group(), population_size)
+
+        elif simulation == ALTERNATING:
             if i % 2 == 0:
                 group = Group.PACK
             else:
                 group = Group.HERD
 
-            table.insert(person, group, DEFAULT_POPULATION_SIZE)
+            table.insert(person, group, population_size)
 
-    elif name == HALVES:
-        for i, person in enumerate(PEOPLE):
-            if i < (len(PEOPLE) / 2):
-                table.insert(person, Group.PACK, DEFAULT_POPULATION_SIZE)
+        elif simulation == HALVES:
+            if i < (num_seats / 2):
+                table.insert(person, Group.PACK, population_size)
             else:
-                table.insert(person, Group.HERD, DEFAULT_POPULATION_SIZE)
+                table.insert(person, Group.HERD, population_size)
 
 
 if __name__ == "__main__":
