8f80f4c0a4c1f74e4b0fc2d4753348f83453f04f
Katherine Erickson <katherine.erickson@gmail.com>
2016-01-31 14:42:29 -0500

    Improve test scenarios to use less code

--- launch.py
+++ launch.py
@@ -2,29 +2,17 @@ import logging
 
 from flask import Flask
 
-from models import Group, Table
+from models import Group, Table, get_random_group
 from localsettings import DEBUG
 
 
 app = Flask(__name__)
 
 
-def get_interaction_html(table):
-    output = '<h3>Initial state</h3>'
-    output += get_seats_as_ul(table.get_seats())
-    table.all_seats_interact(num_generations=5)
-    output += '<h3>Final state</h3>'
-    output += get_seats_as_ul(table.get_seats())
-    return output
-
-
-def get_seats_as_ul(seats):
-    output = '<table>'
-    for seat in seats:
-        output += '<tr><td>{}</td><td>{}</td></tr>'.format(
-            seat, seat.population_size)
-    output += '</table>'
-    return output
+NUM_GENERATIONS = 5
+POPULATION_SIZE = 100
+PEOPLE = ('Alice', 'Bob', 'Carol', 'Django', 'Erlich', 'Freddy',
+          'Georgia', 'Heidi', 'Indigo', 'Jack')
 
 
 @app.route("/")
@@ -36,57 +24,62 @@ def hello_mars():
 def simulate_random():
     logging.basicConfig(level=logging.DEBUG)
 
-    x = Table(1)
-    x.insert('alice', Group.PACK, 100)
-    x.insert('bob', Group.COLONY, 100)
-    x.insert('carol', Group.PACK, 100)
-    x.insert('django', Group.HERD, 100)
-    x.insert('erlich', Group.HERD, 100)
-    x.insert('freddy', Group.HERD, 100)
-    x.insert('georgia', Group.COLONY, 100)
-    x.insert('heidi', Group.PACK, 100)
-    x.insert('indigo', Group.COLONY, 100)
-    x.insert('jack', Group.PACK, 100)
+    table = Table(1)
 
-    return get_interaction_html(x)
+    for person in PEOPLE:
+        table.insert(person, get_random_group(), POPULATION_SIZE)
+
+    return get_interaction_html(table, NUM_GENERATIONS)
 
 
 @app.route("/simulate-alternating")
 def simulate_alternating():
     logging.basicConfig(level=logging.DEBUG)
 
-    x = Table(1)
-    x.insert('alice', Group.PACK, 100)
-    x.insert('bob', Group.HERD, 100)
-    x.insert('carol', Group.PACK, 100)
-    x.insert('django', Group.HERD, 100)
-    x.insert('erlich', Group.PACK, 100)
-    x.insert('freddy', Group.HERD, 100)
-    x.insert('georgia', Group.PACK, 100)
-    x.insert('heidi', Group.HERD, 100)
-    x.insert('indigo', Group.PACK, 100)
-    x.insert('jack', Group.HERD, 100)
+    table = Table(1)
 
-    return get_interaction_html(x)
+    for i, person in enumerate(PEOPLE):
+        if i % 2 == 0:
+            group = Group.PACK
+        else:
+            group = Group.HERD
+
+        table.insert(person, group, POPULATION_SIZE)
+
+    return get_interaction_html(table, NUM_GENERATIONS)
 
 
 @app.route("/simulate-halves")
 def simulate_halves():
     logging.basicConfig(level=logging.DEBUG)
 
-    x = Table(1)
-    x.insert('alice', Group.PACK, 100)
-    x.insert('bob', Group.PACK, 100)
-    x.insert('carol', Group.PACK, 100)
-    x.insert('django', Group.PACK, 100)
-    x.insert('erlich', Group.PACK, 100)
-    x.insert('freddy', Group.HERD, 100)
-    x.insert('georgia', Group.HERD, 100)
-    x.insert('heidi', Group.HERD, 100)
-    x.insert('indigo', Group.HERD, 100)
-    x.insert('jack', Group.HERD, 100)
+    table = Table(1)
+    for i, person in enumerate(PEOPLE):
+        if i < (len(PEOPLE) / 2):
+            table.insert(person, Group.PACK, POPULATION_SIZE)
+        else:
+            table.insert(person, Group.HERD, POPULATION_SIZE)
 
-    return get_interaction_html(x)
+    return get_interaction_html(table, NUM_GENERATIONS)
+
+
+# Helpers
+def get_interaction_html(table, num_generations):
+    output = '<h3>Initial state</h3>'
+    output += get_seats_html(table.get_seats())
+    table.all_seats_interact(num_generations=num_generations)
+    output += '<h3>Final state</h3>'
+    output += get_seats_html(table.get_seats())
+    return output
+
+
+def get_seats_html(seats):
+    output = '<table>'
+    for seat in seats:
+        output += '<tr><td>{}</td><td>{}</td></tr>'.format(
+            seat, seat.population_size)
+    output += '</table>'
+    return output
 
 
 if __name__ == "__main__":
