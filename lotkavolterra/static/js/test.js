a4af9bf5c0bbe338637e4e029790f015ed08c32d
Katherine Erickson <katherine.erickson@gmail.com>
2016-02-07 18:31:54 -0500

    More testing D3

--- lotkavolterra/static/js/test.js
+++ lotkavolterra/static/js/test.js
@@ -1,4 +1,7 @@
-const NUM_CIRCLES = 3;
+const SVG_WIDTH = 1200;
+const SVG_HEIGHT = 1200;
+const MAX_RADIUS = 75;
+const NUM_CIRCLES = 20;
 const TRANSITION_DURATION = 500;
 const EASING_FXN = "easeOutCubic";
 
@@ -6,7 +9,7 @@ const EASING_FXN = "easeOutCubic";
 window.onload = function() {
   var page = d3.select("body").attr("id");
 
-  if (page == "test_d3") {
+  if (page == "d3_random_circles") {
     createCircles();
     changeCircleRadius();
   }
@@ -14,14 +17,32 @@ window.onload = function() {
 
 
 function createCircles() {
+  d3.select("body")
+    .append("svg")
+    .attr("width", SVG_WIDTH)
+    .attr("height", SVG_HEIGHT);
+
+  for (i = 0; i < NUM_CIRCLES; i++) {
+    d3.select("svg")
+      .append("circle")
+      .attr("cx", function() {
+        return MAX_RADIUS + Math.random() * (SVG_WIDTH - 2*MAX_RADIUS);
+      })
+      .attr("cy", function() {
+        return MAX_RADIUS + Math.random() * (SVG_HEIGHT - 2*MAX_RADIUS);
+      });
+  }
+
   colorCircles();
 }
 
 
 function colorCircles(color) {
   var colors = d3.scale.category20();
-  d3.selectAll("circle")
+
+  var circle = d3.selectAll("circle")
     .style("fill", function(d, i) { return colors(i); });
+
 }
 
 function changeCircleRadius() {
