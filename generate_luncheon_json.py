9ce85dbf5f4361bddf5279235d0e7be44efbc94f
Katherine Erickson <katherine.erickson@gmail.com>
2016-04-24 20:18:59 -0400

    Make CSV optional in generating JSON

--- generate_luncheon_json.py
+++ generate_luncheon_json.py
@@ -8,12 +8,12 @@ import sys
 parser = argparse.ArgumentParser(
     description="Create JSON of last year's attendees")
 
-parser.add_argument('csv_file', type=argparse.FileType('r'),
-                    help='CSV of luncheon attendees')
-
 parser.add_argument('json_file', type=argparse.FileType('r'),
                     help=('JSON of additional luncheon attributes: '
                           'name, table coordinates, etc.'))
+
+parser.add_argument('--csv_file', type=argparse.FileType('r'),
+                    help='CSV of luncheon attendees')
 
 args = parser.parse_args()
 
@@ -26,53 +26,65 @@ NUM_TABLES_Y = json_data["numTablesY"]
 TABLE_COORDS = json_data["tableCoords"]
 
 
-# Antiquated way to calculate coordinate, to get a perfect grid
-def add_grid_coords(table_number, table):
-    num = table_number - 1  # Go from 1-indexed to 0-indexed
-    table['x'] = (num % NUM_TABLES_X) / (NUM_TABLES_X - 1.0)
-    table['y'] = (num / NUM_TABLES_X) / (NUM_TABLES_Y - 1.0)
+def create_table(number, name=None):
+    table = OrderedDict()
+    table['number'] = number
+    table['x'], table['y'] = TABLE_COORDS[str(number)]
+    table['people'] = []
+    if name:
+        table['name'] = name
+    return table
 
 
 # Parse the CSV file
 tables = {}  # Keyed on table number
 
-for i, row in enumerate(csv.DictReader(args.csv_file)):
-    # Ignore last name to preserve anonymity in the served json file
-    person_name = row['FIRST NAME']  # + ' ' + row['LAST NAME'][0]
-    table_number = int(row['Table'])
-    table_name = row['TABLE HOST']
+if args.csv_file:
+    for i, row in enumerate(csv.DictReader(args.csv_file)):
+        # Ignore last name to preserve anonymity in the served json file
+        person_name = row['FIRST NAME']  # + ' ' + row['LAST NAME'][0]
+        table_number = int(row['Table'])
+        table_name = row['TABLE HOST']
 
-    if not person_name or not table_number or not table_name:
-        raise ValueError('Error in row {}: value missing'
-                         .format(i))
+        if not person_name or not table_number or not table_name:
+            raise ValueError('Error in row {}: value missing'
+                             .format(i))
 
-    # Fetch table if the table has already been created
-    try:
-        table = tables[table_number]
+        # Fetch table if the table has already been created
+        try:
+            table = tables[table_number]
 
-        # Confirm table name matches previous occurrences
-        previous_name = tables[table_number]['name']
-        if previous_name != table_name:
-            raise ValueError(
-                'Error in row {}: Table {} inconsistenty {} and {}'
-                .format(i, table_number, previous_name, table_name))
+            # Confirm table name matches previous occurrences
+            previous_name = tables[table_number]['name']
+            if previous_name != table_name:
+                raise ValueError(
+                    'Error in row {}: Table {} inconsistenty {} and {}'
+                    .format(i, table_number, previous_name, table_name))
 
-    # Add table if first occurrence
-    except KeyError:
-        table = OrderedDict()
-        table['number'] = table_number
-        table['name'] = table_name
-        table['x'], table['y'] = TABLE_COORDS[str(table_number)]
-        table['people'] = []
-        tables[table_number] = table
+        # Add table if first occurrence
+        except KeyError:
+            table = create_table(table_number, table_name)
+            tables[table_number] = table
 
-    table['people'].append({'name': person_name})
+        table['people'].append({'name': person_name})
 
+    # Tranform dictionary to a list, sorted by table number
+    table_list = []
+    for table_number, table in sorted(tables.iteritems()):
+        table_list.append(table)
 
-# Tranform dictionary to a list, sorted by table number
-table_list = []
-for table_number, table in sorted(tables.iteritems()):
-    table_list.append(table)
+else:
+    table_list = []
+
+    # For each table
+    for i in range(1, 25):
+        table = create_table(i)
+
+        # For each seat
+        for j in range(1, 11):
+            table['people'].append({'name': '{}:{}'.format(i, j)})
+
+        table_list.append(table)
 
 
 # Create net output
@@ -80,5 +92,10 @@ data = OrderedDict()
 data['name'] = LUNCHEON_NAME
 data['numTablesX'] = NUM_TABLES_X
 data['numTablesY'] = NUM_TABLES_Y
+data['showSpecies'] = False
+data['showStage'] = True
+data['stageWidth'] = 0.2
+data['stageHeight'] = 0.17
+data['stageY'] = 0.1
 data['tables'] = table_list
 json.dump(data, sys.stdout, indent=2)
