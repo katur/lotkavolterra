959fb09e29a335ab252041f41c9c9e27d56ae606
Katherine Erickson <katherine.erickson@gmail.com>
2016-04-25 20:30:49 -0400

    Move debugging function defs into module.exports

--- static/js/debugging.js
+++ static/js/debugging.js
@@ -1,37 +1,49 @@
 var d3 = require("d3");
 
 
-function drawRandomCircles(params) {
-  var svg = d3.select("svg");
+module.exports = {
+  /**
+   * Draw random circles.
+   */
+  drawRandomCircles: function(params) {
+    var svg = d3.select("svg");
 
-  // Calculate svg height from width.
-  var svgWidth = parseInt(svg.style("width"), 10);
-  var hwRatio = 1/2;
-  var svgHeight = svgWidth * hwRatio;
-  svg.attr("height", svgHeight);
+    // Calculate svg height from width.
+    var svgWidth = parseInt(svg.style("width"), 10);
+    var hwRatio = 1/2;
+    var svgHeight = svgWidth * hwRatio;
+    svg.attr("height", svgHeight);
 
-  // Calculate the space each table can take up in both x and y dimensions.
-  var tableSpace = svgWidth / 8
-  var tableRadius = tableSpace * 1/3;
+    // Calculate the space each table can take up in both x and y dimensions.
+    var tableSpace = svgWidth / 8
+    var tableRadius = tableSpace * 1/3;
 
-  var seats = [];
-  for (var i = 0; i < params.numCircles; i++) {
-    seats.push(i);
-  }
+    var seats = [];
+    for (var i = 0; i < params.numCircles; i++) {
+      seats.push(i);
+    }
 
-  // Add the circle elements.
-  var circles = svg.selectAll("circle")
-    .data(seats)
-    .enter()
-    .append("circle")
-    .each(function(d, i) {
-      d3.select(this)
-        .attr("cx", Math.random() * svgWidth)
-        .attr("cy", Math.random() * svgHeight)
-        .attr("r", Math.random() * 30);
-    });
-}
+    // Add the circle elements.
+    var circles = svg.selectAll("circle")
+      .data(seats)
+      .enter()
+      .append("circle")
+      .each(function(d, i) {
+        d3.select(this)
+          .attr("cx", Math.random() * svgWidth)
+          .attr("cy", Math.random() * svgHeight)
+          .attr("r", Math.random() * 30);
+      });
+  },
 
+
+  updateRandomCircles: updateRandomCircles
+};
+
+
+/***********
+ * Helpers *
+ ***********/
 
 function updateRandomCircles() {
   var svg = d3.select("svg");
@@ -49,8 +61,3 @@ function updateRandomCircles() {
       }
     });
 }
-
-module.exports = {
-  drawRandomCircles: drawRandomCircles,
-  updateRandomCircles: updateRandomCircles
-};
