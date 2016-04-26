cc9eea45c3a964b366376b71cf5e4148a7b72324
Katherine Erickson <katherine.erickson@gmail.com>
2016-04-25 20:12:23 -0400

    Move view fxn definitions within module.exports

--- static/js/view.js
+++ static/js/view.js
@@ -3,145 +3,146 @@ var constants = require("./constants.js");
 var d3 = require("d3");
 
 
-/**
- * Draw all seats.
- *
- * Call this to draw the initial state.
- */
-function drawSeats(params) {
-  var svg = d3.select("svg");
+module.exports = {
+  /**
+   * Draw all seats.
+   *
+   * Call this to draw the initial state of the simulation.
+   */
+  drawSeats: function(params) {
+    var svg = d3.select("svg");
 
-  // Calculate svg height from width.
-  var svgWidth = parseInt(svg.style("width"), 10);
-  var hwRatio = params.numTablesY / params.numTablesX;
-  var svgHeight = svgWidth * hwRatio;
-  svg.attr("height", svgHeight);
+    // Calculate svg height from width.
+    var svgWidth = parseInt(svg.style("width"), 10);
+    var hwRatio = params.numTablesY / params.numTablesX;
+    var svgHeight = svgWidth * hwRatio;
+    svg.attr("height", svgHeight);
 
-  // Calculate the space each table can take up in both x and y dimensions.
-  var tableSpace = svgWidth / params.numTablesX;
-  var tableRadius = tableSpace * constants.TABLE_SPACE_TO_RADIUS_FACTOR;
+    // Calculate the space each table can take up in both x and y dimensions.
+    var tableSpace = svgWidth / params.numTablesX;
+    var tableRadius = tableSpace * constants.TABLE_SPACE_TO_RADIUS_FACTOR;
 
-  // Add these extra attributes to the seat.
-  // Use underscores to resemble the rest of the Python-derived attributes.
-  for (var i = 0; i < params.seats.length; i++) {
-    seat = params.seats[i];
-    seat.svgWidth = svgWidth;
-    seat.svgHeight = svgHeight;
-    seat.tableSpace = tableSpace;
-    seat.tableRadius = tableRadius;
-  }
+    // Add these extra attributes to the seat.
+    // Use underscores to resemble the rest of the Python-derived attributes.
+    for (var i = 0; i < params.seats.length; i++) {
+      seat = params.seats[i];
+      seat.svgWidth = svgWidth;
+      seat.svgHeight = svgHeight;
+      seat.tableSpace = tableSpace;
+      seat.tableRadius = tableRadius;
+    }
 
-  // Add the circle elements.
-  var circles = svg.selectAll("circle")
-    .data(params.seats)
-    .enter()
-    .append("circle")
-    .each(function(d, i) {
-      var coords = getCoordinates(d);
-      d3.select(this)
-        .attr("cx", coords[0])
-        .attr("cy", coords[1])
-        .attr("r", getRadius(d))
-        .classed(d.group, true);
-    });
-
-
-  if (!params.noText) {
-    // Add the text elements.
-    svg.selectAll("text")
+    // Add the circle elements.
+    var circles = svg.selectAll("circle")
       .data(params.seats)
       .enter()
-      .append("text")
+      .append("circle")
       .each(function(d, i) {
         var coords = getCoordinates(d);
         d3.select(this)
-          .attr("x", coords[0])
-          .attr("y", coords[1])
-          .text(params.showSpecies ? d.shortSpecies : d.firstName)
-          .classed("circle-text", true);
+          .attr("cx", coords[0])
+          .attr("cy", coords[1])
+          .attr("r", getRadius(d))
+          .classed(d.group, true);
+      });
+
+    if (!params.noText) {
+      // Add the text elements.
+      svg.selectAll("text")
+        .data(params.seats)
+        .enter()
+        .append("text")
+        .each(function(d, i) {
+          var coords = getCoordinates(d);
+          d3.select(this)
+            .attr("x", coords[0])
+            .attr("y", coords[1])
+            .text(params.showSpecies ? d.shortSpecies : d.firstName)
+            .classed("circle-text", true);
+        });
+    }
+
+    return circles;
+  },
+
+
+  /**
+   * Draw the stage.
+   */
+  drawStage: function(params) {
+    var svg = d3.select("svg");
+    var svgWidth = parseInt(svg.style("width"), 10);
+    var svgHeight = parseInt(svg.style("height"), 10);
+
+    svg.append("rect")
+      .classed("stage", true)
+      .attr("x", svgWidth * (0.5 - params.stageWidth / 2))
+      .attr("y", svgHeight * params.stageY)
+      .attr("width", svgWidth * params.stageWidth)
+      .attr("height", svgHeight * params.stageHeight);
+
+    svg.append("text")
+      .classed("stage-text", true)
+      .text("STAGE")
+      .attr("x", svgWidth * 0.5)
+      .attr("y", svgHeight * (params.stageY + params.stageHeight / 2))
+      .attr("text-anchor", "middle")
+      .attr("alignment-baseline", "middle");
+  },
+
+
+  /**
+   * Make the "stats" visible.
+   *
+   * The stats are the generation/trial counters and the home nav.
+   * The reason we wanted to be able to control their visibility is
+   * to remove them when making screen capture videos.
+   */
+  displayStats: function(params) {
+    d3.select("#stats").style("display", "block");
+  },
+
+
+  /**
+   * Update seat radii.
+   */
+  updateSeatRadii: function(params) {
+    var duration, delay;
+
+    if (params.reset) {
+      duration = 0;
+      delay = constants.BETWEEN_TRIAL_DELAY;
+    } else {
+      duration = constants.TRANSITION_DURATION;
+      delay = 0;
+    }
+
+    params.circles
+      .transition()
+      .duration(duration)
+      .delay(delay)
+      .ease(constants.EASING_FXN)
+      .attr("r", function(d) {
+        return getRadius(d);
+      })
+      .each("end", function(d, i) {
+        // The callback is only needed once over all nodes
+        if (i == 0) {
+          if (params.showStats) {
+            d3.select(".generation-counter")
+              .text("Generation " + params.generation);
+
+            if (params.reset) {
+              d3.select(".trial-counter")
+                .text("Trial " + params.trial);
+            }
+          }
+
+          params.callback(params.callbackParams);
+        }
       });
   }
-
-  return circles;
-}
-
-
-/**
- * Update seat radii.
- */
-function updateSeatRadii(params) {
-  var duration, delay;
-
-  if (params.reset) {
-    duration = 0;
-    delay = constants.BETWEEN_TRIAL_DELAY;
-  } else {
-    duration = constants.TRANSITION_DURATION;
-    delay = 0;
-  }
-
-  params.circles
-    .transition()
-    .duration(duration)
-    .delay(delay)
-    .ease(constants.EASING_FXN)
-    .attr("r", function(d) {
-      return getRadius(d);
-    })
-    .each("end", function(d, i) {
-      // The callback is only needed once over all nodes
-      if (i == 0) {
-        if (params.showStats) {
-          d3.select(".generation-counter")
-            .text("Generation " + params.generation);
-
-          if (params.reset) {
-            d3.select(".trial-counter")
-              .text("Trial " + params.trial);
-          }
-        }
-
-        params.callback(params.callbackParams);
-      }
-    });
-}
-
-
-/**
- * Draw the stage.
- */
-function drawStage(params) {
-  var svg = d3.select("svg");
-  var svgWidth = parseInt(svg.style("width"), 10);
-  var svgHeight = parseInt(svg.style("height"), 10);
-
-  svg.append("rect")
-    .classed("stage", true)
-    .attr("x", svgWidth * (0.5 - params.stageWidth / 2))
-    .attr("y", svgHeight * params.stageY)
-    .attr("width", svgWidth * params.stageWidth)
-    .attr("height", svgHeight * params.stageHeight);
-
-  svg.append("text")
-    .classed("stage-text", true)
-    .text("STAGE")
-    .attr("x", svgWidth * 0.5)
-    .attr("y", svgHeight * (params.stageY + params.stageHeight / 2))
-    .attr("text-anchor", "middle")
-    .attr("alignment-baseline", "middle")
-}
-
-
-/**
- * Make the "stats" visible.
- *
- * The stats are the generation/trial counters and the home nav.
- * The reason we wanted to be able to control their visibility is
- * to remove them when making screen capture videos.
- */
-function displayStats(params) {
-  d3.select("#stats").style("display", "block");
-}
+};
 
 
 /***********
@@ -185,12 +186,4 @@ function getRadius(seat) {
 
   // Allow seats to get as big as the table at their largest
   return relative * seat.tableRadius;
-}
-
-
-module.exports = {
-  drawSeats: drawSeats,
-  drawStage: drawStage,
-  displayStats: displayStats,
-  updateSeatRadii: updateSeatRadii
 }
